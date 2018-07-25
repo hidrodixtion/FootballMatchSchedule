482e50038ab50ebc72f9a1f7d30b64c623a80e97
Adi Nugroho <hidrodixtion@gmail.com>
2018-07-25 09:08:34 +0700

    Added search schedule.

---
+++ app/src/main/java/com/projectbox/footballmatchschedule/view/SearchScheduleActivity.kt
@@ -0,0 +1,12 @@
+package com.projectbox.footballmatchschedule.view
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SearchScheduleActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_search_schedule)
+    }
+}
